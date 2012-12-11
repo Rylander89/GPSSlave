a27d5e08aad43358f06ae40159babf01b3b7ddf1
Staffan Johansson <staffanbgj@gmail.com>
2012-12-11 16:50:27 +0100

    No change

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=GPSSlave.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/GPSSlave.X.production.hex
+CND_ARTIFACT_NAME_default=GPSSlave.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/GPSSlave.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=gpsslave.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/gpsslave.x.tar
+CND_PACKAGE_NAME_default=gpsslave.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/gpsslave.tar
--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/GPSSlave.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=GPSSlave.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=gpsslave.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/GPSSlave.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=GPSSlave.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=gpsslave/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/gpsslave.x/bin
+makeDirectory ${TMPDIR}/gpsslave/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/gpsslave.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/gpsslave.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/gpsslave.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/gpsslave.tar *
 checkReturnCode
 
 # Cleanup
